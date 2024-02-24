02d4059acff7395321f4addb1f051b4bfb8f11ac
michaelmosch15 <alphonse52leo@gmail.com>
2024-02-23 19:32:32 -0500

    push

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Vestor`,
+    title: `Veztor`,
     siteUrl: `https://www.yourdomain.tld`
   },
   plugins: ["gatsby-plugin-image", "gatsby-plugin-sitemap", {
